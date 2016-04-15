4f8eb0d11da9ab4050f3440641f9a85bd88d28e7
Zhenfang Wei <kopkop@gmail.com>
2016-04-15 22:39:11 +0200

    Moved the hello msg to general package
    Signed-off-by: Zhenfang Wei <kopkop@gmail.com>

--- protocols/ofp10/protocol.go
+++ protocols/ofp10/protocol.go
@@ -119,21 +119,6 @@ const (
 	OfpQueFailedCodeErrPerm        /* Permissions error. */
 )
 
-// OfpHelloMsg represents the hello message structure
-type OfpHelloMsg struct {
-	Header ofpgeneral.OfpHeader
-}
-
-// MarshalBinary converts the hello msg fields into byte array
-func (hello *OfpHelloMsg) MarshalBinary() ([]byte, error) {
-	return (&hello.Header).MarshalBinary()
-}
-
-// UnmarshalBinary transforms the byte array into hello message data
-func (hello *OfpHelloMsg) UnmarshalBinary(data []byte) error {
-	return (&hello.Header).UnmarshalBinary(data)
-}
-
 // OfpPacketInMsg reprensents the packet_in message received by controller
 /* Packet received on port (datapath -> controller). */
 type OfpPacketInMsg struct {
