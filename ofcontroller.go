015b8b9e00ad196658a28e995fe4b57a6b0c9961
Zhenfang Wei <kopkop@gmail.com>
2016-04-19 22:09:55 +0200

    Implement the hello message parsing
    Signed-off-by: Zhenfang Wei <kopkop@gmail.com>

--- ofcontroller.go
+++ ofcontroller.go
@@ -47,11 +47,11 @@ func NewOfpController() (OfpController, error) {
 // StartListen will start a tcp listener
 func (oc *ofpControllerImpl) StartListen(portNo int) {
 	portNoStr := fmt.Sprintf(":%d", portNo)
-	addr, err := net.ResolveTCPAddr("tcp4", portNoStr)
+	addr, err := net.ResolveTCPAddr("tcp", portNoStr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	listener, err := net.ListenTCP("tcp4", addr)
+	listener, err := net.ListenTCP("tcp", addr)
 	if err != nil {
 		log.Fatal(err)
 	}
--- protocols/ofp10/ofpparser.go
+++ protocols/ofp10/ofpparser.go
@@ -1,6 +1,10 @@
 package ofp10
 
-import "github.com/kopwei/goof/protocols/ofpgeneral"
+import (
+	"errors"
+
+	"github.com/kopwei/goof/protocols/ofpgeneral"
+)
 
 // OfpMessageParser is the message parser implementation
 type OfpMessageParser struct {
@@ -8,5 +12,23 @@ type OfpMessageParser struct {
 
 // ParseMsg is used to convert bytes into ofp message
 func (p *OfpMessageParser) ParseMsg(b []byte) (ofpgeneral.OfpMessage, error) {
-	return nil, nil
+	var message ofpgeneral.OfpMessage
+	var err error
+	switch b[1] {
+	case OfpTypeHello:
+		message = &ofpgeneral.OfpHelloMsg{}
+		message.UnmarshalBinary(b)
+	case OfpTypeError:
+		message = &ofpgeneral.OfpErrMsg{}
+		message.UnmarshalBinary(b)
+	case OfpTypeEchoRequest:
+		message = &ofpgeneral.OfpHeader{}
+		message.UnmarshalBinary(b)
+	case OfpTypeEchoReply:
+		message = &ofpgeneral.OfpHeader{}
+		message.UnmarshalBinary(b)
+	default:
+		err = errors.New("An unknown v1.0 packet type was received. Parse function will discard data.")
+	}
+	return message, err
 }
